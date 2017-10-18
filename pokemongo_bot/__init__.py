81232147231e08e4d7a173973016924e7318b87a
rakuuhwa <loki.balazs@hotmail.hu>
2017-10-18 12:11:05 +0200

    Update __init__.py
    Heartbeat function #6229

--- pokemongo_bot/__init__.py
+++ pokemongo_bot/__init__.py
@@ -15,6 +15,7 @@ import threading
 import shelve
 import uuid
 import urllib2
+import calendar
 
 from geopy.geocoders import GoogleV3
 from pgoapi import PGoApi
@@ -1582,7 +1583,9 @@ class PokemonGoBot(object):
                 request.get_inbox(is_history=True, is_reverse=False)
             else:
                 request.get_inbox(is_history=False,is_reverse=True,not_before_ms=self.get_inbox_time)
-            self.get_inbox_time = int(round(time.time() * 1000))
+            # self.get_inbox_time = int(round(time.time() * 1000))
+            #same function but with python and windows support.
+            self.get_inbox_time =calendar.timegm(datetime.datetime.now().timetuple())*1000            
             responses = None
             try:
                 responses = request.call()
